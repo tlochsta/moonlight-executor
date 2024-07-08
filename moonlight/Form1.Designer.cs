1c8b25d54461ce5d78b4221bfe140ba55fe40172
tlochsta <tlochsta@gmail.com>
2024-07-08 16:18:32 -0400

    app upload

---
+++ moonlight/Form1.Designer.cs
@@ -0,0 +1,155 @@
+﻿namespace moonlight
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.webView21 = new Microsoft.Web.WebView2.WinForms.WebView2();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.inject = new System.Windows.Forms.Button();
+            this.execute = new System.Windows.Forms.Button();
+            this.button1 = new System.Windows.Forms.Button();
+            this.notifyIcon1 = new System.Windows.Forms.NotifyIcon(this.components);
+            this.notifyIcon2 = new System.Windows.Forms.NotifyIcon(this.components);
+            ((System.ComponentModel.ISupportInitialize)(this.webView21)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // webView21
+            // 
+            this.webView21.AllowExternalDrop = true;
+            this.webView21.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(33)))), ((int)(((byte)(33)))), ((int)(((byte)(33)))));
+            this.webView21.CreationProperties = null;
+            this.webView21.DefaultBackgroundColor = System.Drawing.Color.White;
+            this.webView21.Location = new System.Drawing.Point(7, 12);
+            this.webView21.Name = "webView21";
+            this.webView21.Size = new System.Drawing.Size(543, 353);
+            this.webView21.TabIndex = 2;
+            this.webView21.ZoomFactor = 1D;
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.Image = ((System.Drawing.Image)(resources.GetObject("pictureBox1.Image")));
+            this.pictureBox1.Location = new System.Drawing.Point(577, 12);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(128, 122);
+            this.pictureBox1.TabIndex = 4;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.Click += new System.EventHandler(this.pictureBox1_Click);
+            // 
+            // inject
+            // 
+            this.inject.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(33)))), ((int)(((byte)(33)))), ((int)(((byte)(33)))));
+            this.inject.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.inject.Font = new System.Drawing.Font("Segoe UI", 17F, System.Drawing.FontStyle.Bold);
+            this.inject.ForeColor = System.Drawing.Color.White;
+            this.inject.Location = new System.Drawing.Point(556, 307);
+            this.inject.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.inject.Name = "inject";
+            this.inject.Size = new System.Drawing.Size(168, 58);
+            this.inject.TabIndex = 0;
+            this.inject.Text = "Inject";
+            this.inject.UseVisualStyleBackColor = false;
+            this.inject.Click += new System.EventHandler(this.inject_Click);
+            // 
+            // execute
+            // 
+            this.execute.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(33)))), ((int)(((byte)(33)))), ((int)(((byte)(33)))));
+            this.execute.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.execute.Font = new System.Drawing.Font("Segoe UI", 17F, System.Drawing.FontStyle.Bold);
+            this.execute.ForeColor = System.Drawing.Color.White;
+            this.execute.Location = new System.Drawing.Point(556, 243);
+            this.execute.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.execute.Name = "execute";
+            this.execute.Size = new System.Drawing.Size(168, 58);
+            this.execute.TabIndex = 1;
+            this.execute.Text = "Execute";
+            this.execute.UseVisualStyleBackColor = false;
+            this.execute.Click += new System.EventHandler(this.execute_Click);
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(33)))), ((int)(((byte)(33)))), ((int)(((byte)(33)))));
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Popup;
+            this.button1.Font = new System.Drawing.Font("Segoe UI", 17F, System.Drawing.FontStyle.Bold);
+            this.button1.ForeColor = System.Drawing.Color.White;
+            this.button1.Location = new System.Drawing.Point(557, 179);
+            this.button1.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(167, 58);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Website";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // notifyIcon1
+            // 
+            this.notifyIcon1.Text = "notifyIcon1";
+            this.notifyIcon1.Visible = true;
+            // 
+            // notifyIcon2
+            // 
+            this.notifyIcon2.Text = "notifyIcon2";
+            this.notifyIcon2.Visible = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(13)))), ((int)(((byte)(13)))), ((int)(((byte)(13)))));
+            this.ClientSize = new System.Drawing.Size(729, 377);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.webView21);
+            this.Controls.Add(this.execute);
+            this.Controls.Add(this.inject);
+            this.Font = new System.Drawing.Font("Segoe UI", 8.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Margin = new System.Windows.Forms.Padding(4, 3, 4, 3);
+            this.Name = "Form1";
+            this.Text = "moonlight executor";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.webView21)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private Microsoft.Web.WebView2.WinForms.WebView2 webView21;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Button inject;
+        private System.Windows.Forms.Button execute;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.NotifyIcon notifyIcon1;
+        private System.Windows.Forms.NotifyIcon notifyIcon2;
+    }
+}
+
---
+++ moonlight/Form1.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.IO;
+using System.Windows.Forms;
+using Microsoft.Web.WebView2.WinForms;
+using Microsoft.Web.WebView2.Core;
+using System.Text.Json;
+using CeleryAPI;
+using System.Runtime.InteropServices;
+using System.Diagnostics;
+
+
+namespace moonlight
+{
+    public partial class Form1 : Form
+    {
+        public const int WM_NCLBUTTONDOWN = 0xA1;
+        public const int HTCAPTION = 0x2;
+        [DllImport("User32.dll")]
+        public static extern bool ReleaseCapture();
+        [DllImport("User32.dll")]
+        public static extern int SendMessage(IntPtr hWnd, int Msg, int wParam, int lParam);
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private async void execute_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                string script = "window.getEditorContent();";
+                var result = await webView21.CoreWebView2.ExecuteScriptAsync(script);
+                string editorContent = System.Text.Json.JsonSerializer.Deserialize<string>(result); // Deserialize JSON result
+                CeleryAPI.ByfronPlayer.execute(editorContent);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Error: " + ex.Message);
+            }
+        }
+
+        private void inject_Click(object sender, EventArgs e)
+        {
+            CeleryAPI.ByfronPlayer.Inject();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            string url = "https://moonlight.dawg.tf";
+            Process.Start(url);
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            webView21.Source = new Uri(Path.Combine(Application.StartupPath, @"Monaco\index.html"));
+        }
+
+
+        private async void InitializeWebView()
+        {
+            await webView21.EnsureCoreWebView2Async(null);
+        }
+
+        private void pictureBox1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+        }
+        private void OnMouseDown(object sender, MouseEventArgs e)
+        {
+            if (e.Button == MouseButtons.Left)
+            {
+                ReleaseCapture();
+                SendMessage(Handle, WM_NCLBUTTONDOWN, HTCAPTION, 0);
+            }
+        }
+        private void minimizeButton_Click(object sender, System.EventArgs e)
+        {
+            WindowState = FormWindowState.Minimized;
+        }
+
+        private void maximizeButton_Click(object sender, System.EventArgs e)
+        {
+            if (this.WindowState != FormWindowState.Maximized)
+                this.WindowState = FormWindowState.Maximized;
+            else
+                this.WindowState = FormWindowState.Normal;
+        }
+
+        private void closeButton_Click(object sender, System.EventArgs e)
+        {
+            Close();
+        }
+    }
+}
